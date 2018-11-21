89eee7ba847cf5acbc2ba6c275d759fca84aeb7d
korotushak96 <42374826+korotushak96@users.noreply.github.com>
2018-11-21 13:16:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    let tabHeader = document.querySelector('.info-header'),
+        tabs = document.querySelectorAll('.info-header-tab'),
+        tabContent = document.querySelectorAll('.info-tabcontent')
+
+   function hide(a){
+       for (let i = a; i<tabContent.length; i++){
+           
+       }
+   }
+})
