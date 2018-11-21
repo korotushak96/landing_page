b1b6249e9c1088fab1542f58577e7a31268c6da6
korotushak96 <42374826+korotushak96@users.noreply.github.com>
2018-11-21 13:43:38 +0200

    add tabs

--- js/script.js
+++ js/script.js
@@ -4,9 +4,32 @@ window.addEventListener('DOMContentLoaded', function(){
         tabs = document.querySelectorAll('.info-header-tab'),
         tabContent = document.querySelectorAll('.info-tabcontent')
 
-   function hide(a){
+   function hideTab(a){
        for (let i = a; i<tabContent.length; i++){
-           
+             tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide')
+             
+        }
+   }
+   hideTab(1);
+
+   function showTab(b){
+       if (tabContent[b].classList.contains('hide')){
+           tabContent[b].classList.remove('hide');
+           tabContent[b].classList.add('show');
        }
    }
+
+   tabHeader.addEventListener('click', function(event){
+       let target = event.target;
+       if(target && target.classList.contains('info-header-tab')){
+           for (let i=0; i<tabs.length; i++){
+               if (tabs[i]==target) {
+                   hideTab(0);
+                   showTab(i);
+               }
+           }
+       }
+   })
+
 })
