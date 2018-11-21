dddbd438878f0c19dc7c36d48ca38e2f0410aaa5
korotushak96 <42374826+korotushak96@users.noreply.github.com>
2018-11-21 17:07:43 +0200

    add timer

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ window.addEventListener('DOMContentLoaded', function(){
 
     let tabHeader = document.querySelector('.info-header'),
         tabs = document.querySelectorAll('.info-header-tab'),
-        tabContent = document.querySelectorAll('.info-tabcontent')
+        tabContent = document.querySelectorAll('.info-tabcontent');
 
    function hideTab(a){
        for (let i = a; i<tabContent.length; i++){
              tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide')
-             
-        }
-   }
+            tabContent[i].classList.add('hide');
+        };
+   };
    hideTab(1);
 
    function showTab(b){
@@ -18,7 +17,7 @@ window.addEventListener('DOMContentLoaded', function(){
            tabContent[b].classList.remove('hide');
            tabContent[b].classList.add('show');
        }
-   }
+   };
 
    tabHeader.addEventListener('click', function(event){
        let target = event.target;
@@ -27,9 +26,53 @@ window.addEventListener('DOMContentLoaded', function(){
                if (tabs[i]==target) {
                    hideTab(0);
                    showTab(i);
-               }
-           }
-       }
-   })
+               };
+           };
+       };
+   });
 
+
+   // timer
+
+   let deadLine = '2018-11-22';
+
+   function makeInterval(deadLine){
+        let t = Date.parse(deadLine) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/1000/60/60));
+            if (seconds<10) seconds = '0' + seconds;
+            if (minutes<10) minutes = '0' + minutes;
+            if (hours<10) hours = '0' + hours;    
+        if (t<0) return {
+            'total': 0,
+            'seconds': '00',
+            'minutes': '00',
+            'hours': '00'
+        }
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        }
+   };
+
+   function setClock(id, deadLine){
+       let  clock = document.getElementById(id),
+            seconds = clock.querySelector('.seconds'),
+            minutes = clock.querySelector('.minutes'),
+            hours = clock.querySelector('.hours'),
+            timer = setInterval(setTimer, 1000);
+
+        function setTimer(){
+            let t = makeInterval(deadLine);
+            seconds.textContent = t.seconds;
+            minutes.textContent = t.minutes;
+            hours.textContent = t.hours
+            if (t.total<=0) clearInterval(timer);
+        };
+   };
+
+   setClock('timer', deadLine)
 })
